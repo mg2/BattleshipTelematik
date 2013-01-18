fe0701020962f94fe506c0461c0665f4283d21e2
Dimitar Gruev <mr.gruev@gmail.com>
2013-01-18 23:57:40 +0100

    changess

--- BattleShipWPF/BattleShipWPF/GameWindow.xaml.cs
+++ BattleShipWPF/BattleShipWPF/GameWindow.xaml.cs
@@ -22,10 +22,14 @@ namespace BattleShipWPF
         Brush set_mouseOver = new SolidColorBrush(System.Windows.Media.Colors.Red);
         Brush water = new SolidColorBrush(System.Windows.Media.Colors.Blue);
         Brush ship = new SolidColorBrush(System.Windows.Media.Colors.Black);
+        Brush miss = new SolidColorBrush(System.Windows.Media.Colors.White);
 
         private int[] gameField = new int[100];
+        private int[] opponentGameField = new int[100];
         Rectangle[] playerFieldRect = new Rectangle[100];
         Rectangle[] opponentFieldRect = new Rectangle[100];
+
+        bool yourTurn = true;
 
         public GameWindow()
         {
@@ -56,6 +60,7 @@ namespace BattleShipWPF
             opponentField.ShowGridLines = true;
             for (int i = 0; i < 100; i++)
             {
+                opponentGameField[i] = 0;
                 opponentFieldRect[i] = new Rectangle();
                 opponentFieldRect[i].Fill = water;
 
@@ -63,6 +68,139 @@ namespace BattleShipWPF
                 Grid.SetColumn(opponentFieldRect[i], i % 10);
 
                 opponentField.Children.Add(opponentFieldRect[i]);
+                opponentFieldRect[i].MouseLeftButtonUp += opponentFieldRect_MouseLeftButtonUp;
+            }
+        }
+
+        private void opponentFieldRect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int index = Array.IndexOf(opponentFieldRect, (Rectangle)sender);
+            //x = index % 10, y = index / 10
+
+            //Not your turn
+            if (yourTurn == false) logText.Text = "Not your turn!\n" + logText.Text;
+            //Invalid field
+            else if (opponentGameField[index] != 0) logText.Text = "Invalid shot!\n" + logText.Text;
+            else
+            {
+                //Send to server x, y
+                String shotString = "SHOOT " + (index % 10).ToString() + "," + (index / 10).ToString() + "\r\n";
+
+                //Wait for response
+                //call 
+                
+            }
+        }
+
+        private void onServerResult(String result)
+        {
+            String resultString = "RESULT hit 4,4\r\n"; //HARDCODED!!!
+            //String resultString = result;
+
+            resultString.Replace("\r\n", "");
+            String[] serverResult = resultString.Split(' ');
+
+            int x = Convert.ToInt32(serverResult[2].Split(',')[0]);
+            int y = Convert.ToInt32(serverResult[2].Split(',')[1]);
+            int index =  x + 10 * y;
+
+            if ((serverResult[0] + " " + serverResult[1]).Equals("TURN FALSE"))
+            {
+                logText.Text = "Not your turn!\n" + logText.Text;
+                yourTurn = false;
+            }
+            else if (yourTurn == true)
+            {
+                //reject
+                if (serverResult[0].Equals("REJECT"))
+                {
+                    logText.Text = "Shot rejected from server!\n" + logText.Text;
+                }
+                //hit
+                else if (serverResult[1].Equals("hit"))
+                {
+                    logText.Text = "Hit!\n" + logText.Text;
+                    Rectangle me = opponentFieldRect[index];
+                    me.Fill = ship;
+                    opponentGameField[index] = 1;
+                }
+                //miss
+                else if (serverResult[1].Equals("miss"))
+                {
+                    logText.Text = "Miss!\n" + logText.Text;
+                    Rectangle me = opponentFieldRect[index];
+                    me.Fill = miss;
+                    yourTurn = false;
+                }
+                //sunk
+                else if (serverResult[1].Equals("sunk"))
+                {
+                    logText.Text = "Ship sunk!\n" + logText.Text;
+                    Rectangle me = opponentFieldRect[index];
+                    me.Fill = ship;
+                    opponentGameField[index] = 1;
+                }
+                else
+                {
+                    // ???
+                }
+            }
+            else
+            {
+                //not your turn!
+                if (serverResult[1].Equals("hit"))
+                {
+                    logText.Text = "Hit!\n" + logText.Text;
+                    Rectangle me = opponentFieldRect[index];
+                    me.Fill = ship;
+                    opponentGameField[index] = 1;
+                }
+                //miss
+                else if (serverResult[1].Equals("miss"))
+                {
+                    logText.Text = "Miss!\n" + logText.Text;
+                    Rectangle me = opponentFieldRect[index];
+                    me.Fill = miss;
+                    yourTurn = false;
+                }
+                //sunk
+                else if (serverResult[1].Equals("sunk"))
+                {
+                    logText.Text = "Ship sunk!\n" + logText.Text;
+                    Rectangle me = opponentFieldRect[index];
+                    me.Fill = ship;
+                    opponentGameField[index] = 1;
+                }
+                else
+                {
+                    if (serverResult[1].Equals("hit"))
+                    {
+                        logText.Text = "Oponent hit on " +  + ".\n" + logText.Text;
+                        Rectangle me = opponentFieldRect[index];
+                        me.Fill = ship;
+                        opponentGameField[index] = 1;
+                    }
+                    //miss
+                    else if (serverResult[1].Equals("miss"))
+                    {
+                        logText.Text = "Miss!\n" + logText.Text;
+                        Rectangle me = opponentFieldRect[index];
+                        me.Fill = miss;
+                        yourTurn = false;
+                    }
+                    //sunk
+                    else if (serverResult[1].Equals("sunk"))
+                    {
+                        logText.Text = "Ship sunk!\n" + logText.Text;
+                        Rectangle me = opponentFieldRect[index];
+                        me.Fill = ship;
+                        opponentGameField[index] = 1;
+                    }
+                    else
+                    {
+                        // ???
+                    }
+                }
             }
         }
     }
